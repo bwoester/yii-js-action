29cc0b9b3a9172353f737c28e2b201612624fcfa
libra guest <benjamin.woester@gmail.com>
2012-08-25 09:31:11 -0400

    use jsAction in sampleApp

--- sampleApp/protected/controllers/SiteController.php
+++ sampleApp/protected/controllers/SiteController.php
@@ -18,6 +18,9 @@ class SiteController extends Controller
 			'page'=>array(
 				'class'=>'CViewAction',
 			),
+            'jsActionDemo'=>array(
+                'class' => 'jsAction.action.Demo',
+            ),
 		);
 	}
 
